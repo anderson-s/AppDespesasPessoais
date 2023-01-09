c8551c98f5270cba8a690b2270970098956a15ea
Andeson Silva <andsilva607@gmail.com>
2023-01-09 13:22:57 -0300

    Adicionando tema

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-
- import 'package:despesas_pessoais/view/home.dart';
+import 'package:despesas_pessoais/view/home.dart';
 import 'package:flutter/material.dart';
 
 main() {
-   runApp(const Inicio());
+  runApp(const Inicio());
 }
 
 class Inicio extends StatelessWidget {
@@ -11,9 +10,31 @@ class Inicio extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final ThemeData tema = ThemeData();
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: const Home(),
+      theme: tema.copyWith(
+        colorScheme: tema.colorScheme.copyWith(
+          primary: Colors.purple,
+          secondary: Colors.amber,
+        ),
+        // textTheme: tema.textTheme.copyWith(
+        //   headline6: const TextStyle(
+        //     fontFamily: 'OpenSans',
+        //     fontSize: 18,
+        //     fontWeight: FontWeight.bold,
+        //     color: Colors.black,
+        //   ),
+        // ),
+        // appBarTheme: const AppBarTheme(
+        //   titleTextStyle: TextStyle(
+        //     fontFamily: 'OpenSans',
+        //     fontSize: 20,
+        //     fontWeight: FontWeight.bold,
+        //   ),
+        // ),
+      ),
     );
   }
 }
