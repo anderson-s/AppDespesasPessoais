3972335eea2cfc3db2d1f0dc3563c35f40c0b84a
Andeson Silva <andsilva607@gmail.com>
2023-01-09 11:41:22 -0300

    Criando modal para exibir formulário de cadastro de transações

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,8 +1,43 @@
-import 'package:despesas_pessoais/view/components/transacoes_usuario.dart';
+import 'package:despesas_pessoais/model/transacao.dart';
+import 'package:despesas_pessoais/view/components/cadastro_de_transacoes.dart';
+import 'package:despesas_pessoais/view/components/lista_de_transacoes.dart';
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var transacoes = [
+    Transacao(
+      id: "t1",
+      titulo: "Transacao 1",
+      valor: 200.00,
+      data: DateTime.now(),
+    ),
+  ];
+
+  _abrirModalForm(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return CadastroTransacoes(cadastrar: adicionar);
+        });
+  }
+
+  adicionar(String titulo, double valor) {
+    final novaTransacao = Transacao(
+        id: DateTime.now().microsecondsSinceEpoch.toString(),
+        titulo: titulo,
+        valor: valor,
+        data: DateTime.now());
+    setState(() {
+      transacoes.add(novaTransacao);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,30 +45,42 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Despesas Pessoais"),
         centerTitle: true,
-        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
+        actions: [
+          IconButton(
+            onPressed: () {
+              _abrirModalForm(context);
+            },
+            icon: const Icon(Icons.add),
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
           // mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment:
               CrossAxisAlignment.stretch, //Ocupa a largura inteira
-          children: const [
-            SizedBox(
+          children: [
+            const SizedBox(
               child: Card(
                 color: Colors.red,
                 elevation: 5,
                 child: Text("Gráfico"),
               ),
             ),
-            TransacoesUsuario()
+            Column(
+              children: [
+                ListasTransacoes(listaTransacoes: transacoes),
+              ],
+            ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          _abrirModalForm(context);
+        },
         child: const Icon(Icons.add),
       ),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
